499c13c6dd71ffcdea1255c7c98d54ebae6e4b3e
JoaoTeixeira-081 <joao.manuel2000@hotmail.com>
2022-11-21 18:11:06 +0000

    teeste

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const express1 = require('express1')
 const app = express()
-const app1 = express()
+const app1 = express1()
 
-app.get('/', (req, res) => res.send('Hello World!!'))
+app.get('/', (req, res) => res.send('Hello World 1111!!'))
 app1.get('/', (req, res) => res.send('I am João Teixeira!!'))
 app.listen(3000, () => console.log('Server ready'))
+app1.listen(3000, () => console.log('Server ready'))
