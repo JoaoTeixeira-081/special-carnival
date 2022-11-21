f52f143c6b0f5335ac90469922d140e423c6d203
JoaoTeixeira-081 <joao.manuel2000@hotmail.com>
2022-11-21 17:50:14 +0000

    Change apps.js #2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
+const app1 = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/', (req, res) => res.send('I am João Teixeira!!'))
+app.get('/', (req, res) => res.send('Hello World!!'))
+app1.get('/', (req, res) => res.send('I am João Teixeira!!'))
 app.listen(3000, () => console.log('Server ready'))
